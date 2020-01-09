3ca4836d5bcad95cfd972f5ef4635c3fa3b7e3b1
ndgithub <nickydesai.mail@gmail.com>
2020-01-09 14:42:43 -0600

    Minor refactoring

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,6 @@ import Sidebar from './Sidebar';
 import Micrograph from './Micrograph';
 import html2canvas from 'html2canvas';
 
-
 class Main extends React.Component {
   constructor(props) {
     super(props);
@@ -27,24 +26,18 @@ class Main extends React.Component {
 
       scalebarTextColor: '#000000',
       scalebarBgColor: '#ffffff',
-
       isDrawLineinProg: false,
       currDrawLinePts: null,
       drawLines: null
-
-
     };
 
     this.origDims = null;
     this.isMouseDown = null;
     this.lastMousePos = null;
-
-
-
   }
 
   useDemoUpload = () => {
-    var url = 'https://i.imgur.com/ssPGfDJ.jpg'
+    var url = 'https://i.imgur.com/ssPGfDJ.jpg';
     var img = new Image();
     img.onload = () => {
       this.origDims = { width: img.width, height: img.height };
@@ -58,19 +51,18 @@ class Main extends React.Component {
         origDims: { width: img.width, height: img.height },
         imageLoaded: true,
 
-
         isImageScaleSet: false,
         isScalebarChecked: false,
         inputLengthValue: '',
         inputUnitsValue: '',
         scalePts: []
       });
-    }
+    };
     img.src = url;
-  }
+  };
   // Called from Sidebar when user uploads file
-  handleFileUpload = (event) => {
-    console.log('handlefileUpload called')
+  handleFileUpload = event => {
+    console.log('handlefileUpload called');
     if (event.target.files.length === 0) return;
     var url = URL.createObjectURL(event.target.files[0]);
     var img = new Image();
@@ -92,13 +84,13 @@ class Main extends React.Component {
         inputUnitsValue: '',
         scalePts: []
       });
-    }
+    };
     img.src = url;
   };
 
-
   getInitSize = () => {
-    var ratioY = this.origDims.height / this.state.containerRef.current.offsetHeight;
+    var ratioY =
+      this.origDims.height / this.state.containerRef.current.offsetHeight;
     var initSizeY = this.origDims.height / ratioY;
     var initSizeX = this.origDims.width / ratioY;
 
@@ -107,124 +99,141 @@ class Main extends React.Component {
       initSizeX = this.state.containerRef.current.offsetWidth;
       initSizeY = initSizeY / newRatioX;
     }
-    return { width: initSizeX, height: initSizeY }
-  }
+    return { width: initSizeX, height: initSizeY };
+  };
 
-  getInitPos = (size) => {
-
+  getInitPos = size => {
     var posX = (this.state.containerRef.current.offsetWidth - size.width) / 2;
     var posY = (this.state.containerRef.current.offsetHeight - size.height) / 2;
     return { x: posX, y: posY };
-  }
+  };
 
   getNewPosition = (containerRef, oldSize, oldPos, newSize) => {
     let newPos = {};
-    let old_containerCenterX_relToImgCorner_percent = ((containerRef.current.offsetWidth / 2) - oldPos.x) / oldSize.width;
-    let old_containerCenterY_relToImgCorner_percent = ((containerRef.current.offsetHeight / 2) - oldPos.y) / oldSize.height;
+    let old_containerCenterX_relToImgCorner_percent =
+      (containerRef.current.offsetWidth / 2 - oldPos.x) / oldSize.width;
+    let old_containerCenterY_relToImgCorner_percent =
+      (containerRef.current.offsetHeight / 2 - oldPos.y) / oldSize.height;
 
-    let new_pointToKeepCenteredX_ReltoContainer_pixels = (old_containerCenterX_relToImgCorner_percent * newSize.width) + oldPos.x;
-    let offsetX = new_pointToKeepCenteredX_ReltoContainer_pixels - (containerRef.current.offsetWidth / 2);
+    let new_pointToKeepCenteredX_ReltoContainer_pixels =
+      old_containerCenterX_relToImgCorner_percent * newSize.width + oldPos.x;
+    let offsetX =
+      new_pointToKeepCenteredX_ReltoContainer_pixels -
+      containerRef.current.offsetWidth / 2;
     newPos.x = oldPos.x - offsetX;
 
-    let new_pointToKeepCenteredY_ReltoContainer_pixels = (old_containerCenterY_relToImgCorner_percent * newSize.height) + oldPos.y;
-    let offsetY = new_pointToKeepCenteredY_ReltoContainer_pixels - (containerRef.current.offsetHeight / 2);
+    let new_pointToKeepCenteredY_ReltoContainer_pixels =
+      old_containerCenterY_relToImgCorner_percent * newSize.height + oldPos.y;
+    let offsetY =
+      new_pointToKeepCenteredY_ReltoContainer_pixels -
+      containerRef.current.offsetHeight / 2;
     newPos.y = oldPos.y - offsetY;
 
     return newPos;
-
-  }
+  };
 
   /////////////  Prop functions called from Micrograph component ///////////////
-  getContainerRef = (ref) => {
-    this.setState({ containerRef: ref })
-  }
+  getContainerRef = ref => {
+    this.setState({ containerRef: ref });
+  };
 
-
-
-  onMouseScroll = (e) => {
+  onMouseScroll = e => {
     let newSize = {};
-    newSize.width = this.state.size.width * (1 - (0.001 * e.deltaY));
-    newSize.height = this.state.size.height * (1 - (0.001 * e.deltaY));
+    newSize.width = this.state.size.width * (1 - 0.001 * e.deltaY);
+    newSize.height = this.state.size.height * (1 - 0.001 * e.deltaY);
 
-    let newPos = this.getNewPosition(this.state.containerRef, this.state.size, this.state.pos, newSize)
+    let newPos = this.getNewPosition(
+      this.state.containerRef,
+      this.state.size,
+      this.state.pos,
+      newSize
+    );
 
     this.setState({
       size: newSize,
-      pos: newPos,
-    })
+      pos: newPos
+    });
+  };
 
-  }
-
-  onCheckUseScalebar = (checked) => {
+  onCheckUseScalebar = checked => {
     this.setState({
       isScalebarChecked: checked
     });
 
     // if checked and scalebar is already set, use scalebar - this is new.
-    // if not it is in progress. 
-
-  }
+    // if not it is in progress.
+  };
 
   onClickCancelSetting = () => {
     this.setState({
-      isScaleSetInProg: false,
-
-    })
-  }
+      isScaleSetInProg: false
+    });
+  };
 
   onClickDoneSetting = () => {
-
-    let imgScalePerc = Math.abs(this.state.scalePts[0].x - this.state.scalePts[1].x);
+    let imgScalePerc = Math.abs(
+      this.state.scalePts[0].x - this.state.scalePts[1].x
+    );
     let imgSizeUnits = this.state.inputLengthValue / imgScalePerc;
     this.setState({
       imgSizeUnits: imgSizeUnits,
       isImageScaleSet: true,
       units: this.state.inputUnitsValue,
       isScaleSetInProg: false,
-      isScalebarChecked: true,
-    })
+      isScalebarChecked: true
+    });
+  };
 
-  }
-
-
-
-  onInputLengthChange = (e) => {
+  onInputLengthChange = e => {
     e.preventDefault();
     this.setState({
-      inputLengthValue: e.target.value,
+      inputLengthValue: e.target.value
+    });
+  };
 
-    })
-
-  }
-
-  onInputUnitsChange = (e) => {
+  onInputUnitsChange = e => {
     e.preventDefault();
     this.setState({
-      inputUnitsValue: e.target.value,
-
-    })
-  }
+      inputUnitsValue: e.target.value
+    });
+  };
 
   wasDragged = () => {
-    let dragged = (this.lastMouseDownPos.x !== this.lastMouseUpPos.x && this.lastMouseDownPos.y !== this.lastMouseUpPos.y);
+    let dragged =
+      this.lastMouseDownPos.x !== this.lastMouseUpPos.x &&
+      this.lastMouseDownPos.y !== this.lastMouseUpPos.y;
 
     return dragged;
-  }
-  convertToImgPos = (pagePos) => {
+  };
+  convertToImgPos = pagePos => {
     // The posisitoin on the page minus the offset of the container and minuse the offset of the image all divided by image widht
-    var imgPtX = (pagePos.x - this.state.containerRef.current.offsetLeft - this.state.pos.x) / this.state.size.width;
-    var imgPtY = (pagePos.y - this.state.containerRef.current.offsetTop - this.state.pos.y) / this.state.size.height;
+    var imgPtX =
+      (pagePos.x -
+        this.state.containerRef.current.offsetLeft -
+        this.state.pos.x) /
+      this.state.size.width;
+    var imgPtY =
+      (pagePos.y -
+        this.state.containerRef.current.offsetTop -
+        this.state.pos.y) /
+      this.state.size.height;
     return { x: imgPtX, y: imgPtY };
-  }
+  };
 
   onSaveSnapClicked = () => {
-    html2canvas(this.state.containerRef.current, { logging: false, useCORS: true }).then(canvas => {
+    html2canvas(this.state.containerRef.current, {
+      logging: false,
+      useCORS: true
+    }).then(canvas => {
       let canvDataUrl = canvas.toDataURL();
-      this.setState(prevState => ({
-        snapUrls: [...prevState.snapUrls, canvDataUrl]
-      }), () => console.log(this.state.snapUrls.length));
+      this.setState(
+        prevState => ({
+          snapUrls: [...prevState.snapUrls, canvDataUrl]
+        }),
+        () => console.log(this.state.snapUrls.length)
+      );
     });
-  }
+  };
 
   onClickSetImageScale = () => {
     this.setState({
@@ -233,181 +242,200 @@ class Main extends React.Component {
       inputLengthValue: '',
       inputUnitsValue: '',
       scalePts: []
-    })
-  }
+    });
+  };
 
-  onClickScaleTextColor = (color) => {
+  onClickScaleTextColor = color => {
     this.setState({
-      scalebarTextColor: color,
-    })
-  }
+      scalebarTextColor: color
+    });
+  };
 
-  onClickScaleBgColor = (color) => {
+  onClickScaleBgColor = color => {
     this.setState({
-      scalebarBgColor: color,
-    })
-  }
+      scalebarBgColor: color
+    });
+  };
 
-
-  mouseDown = (e) => {
+  mouseDown = e => {
     this.isMouseDown = true;
     this.lastMouseDownPos = {
       x: e.pageX,
       y: e.pageY
-    }
+    };
     if (this.state.isDrawLineinProg) {
-      this.setState({
-        currDrawLinePts: [this.lastMouseDownPos]
-      }, () => console.log(this.state.currDrawLinePts))
+      this.setState(
+        {
+          currDrawLinePts: [this.lastMouseDownPos]
+        },
+        () => console.log(this.state.currDrawLinePts)
+      );
     } else {
       this.setState({
-        cursorStyle: ((this.state.isScaleSetInProg && this.state.scalePts.length < 2) ? 'crosshair' : 'move')
-      })
+        cursorStyle:
+          this.state.isScaleSetInProg && this.state.scalePts.length < 2
+            ? 'crosshair'
+            : 'move'
+      });
     }
+  };
 
-
-  }
-
-  mouseUp = (e) => {
-
+  mouseUp = e => {
     this.isMouseDown = false;
     this.lastMouseUpPos = {
       x: e.pageX,
       y: e.pageY
-    }
-    // if scalebar setting is in progress and wasn't dragged, its a click pt. 
-    if ((this.state.isScaleSetInProg)) {
-      this.setState(prevState => ({
-        scalePts: [...prevState.scalePts, this.convertToImgPos(this.lastMouseUpPos)]
-      }), () => {
-        this.setState({
-          cursorStyle: ((this.state.isScaleSetInProg && this.state.scalePts.length < 2 ? 'crosshair' : 'auto'))
-        })
-      });
+    };
+    // if scalebar setting is in progress and wasn't dragged, its a click pt.
+    if (this.state.isScaleSetInProg) {
+      this.setState(
+        prevState => ({
+          scalePts: [
+            ...prevState.scalePts,
+            this.convertToImgPos(this.lastMouseUpPos)
+          ]
+        }),
+        () => {
+          this.setState({
+            cursorStyle:
+              this.state.isScaleSetInProg && this.state.scalePts.length < 2
+                ? 'crosshair'
+                : 'auto'
+          });
+        }
+      );
     }
 
-    if ((this.state.isDrawLineinProg) && !this.wasDragged()) {
-      this.setState(prevState => ({
-        scalePts: [...prevState.scalePts, this.convertToImgPos(this.lastMouseUpPos)]
-      }), () => {
-        this.setState({
-          cursorStyle: ((this.state.isScaleSetInProg && this.state.scalePts.length < 2 ? 'crosshair' : 'auto'))
-        })
-      });
+    if (this.state.isDrawLineinProg && !this.wasDragged()) {
+      this.setState(
+        prevState => ({
+          scalePts: [
+            ...prevState.scalePts,
+            this.convertToImgPos(this.lastMouseUpPos)
+          ]
+        }),
+        () => {
+          this.setState({
+            cursorStyle:
+              this.state.isScaleSetInProg && this.state.scalePts.length < 2
+                ? 'crosshair'
+                : 'auto'
+          });
+        }
+      );
     }
 
     this.setState({
-      cursorStyle: ((this.state.isScaleSetInProg && this.state.scalePts.length < 2 ? 'crosshair' : 'auto')),
-      isDrawLineinProg: false,
-    })
+      cursorStyle:
+        this.state.isScaleSetInProg && this.state.scalePts.length < 2
+          ? 'crosshair'
+          : 'auto',
+      isDrawLineinProg: false
+    });
+  };
 
-
-  }
-
-  mouseEnter = (e) => {
+  mouseEnter = e => {
     this.setState({
-      cursorStyle: ((this.state.isScaleSetInProg && this.state.scalePts.length < 2) || this.state.isDrawLineinProg ? 'crosshair' : 'auto')
-    })
-  }
+      cursorStyle:
+        (this.state.isScaleSetInProg && this.state.scalePts.length < 2) ||
+        this.state.isDrawLineinProg
+          ? 'crosshair'
+          : 'auto'
+    });
+  };
 
-
-  mouseMove = (e) => {
+  mouseMove = e => {
     if (this.isMouseDown && !this.state.isDrawLineinProg) {
       let diffX = this.lastMousePos.x - e.pageX;
       let diffY = this.lastMousePos.y - e.pageY;
-      this.lastMousePos = { x: e.pageX, y: e.pageY }
+      this.lastMousePos = { x: e.pageX, y: e.pageY };
       this.setState({
         pos: {
           x: this.state.pos.x - diffX,
           y: this.state.pos.y - diffY
         },
         cursorStyle: 'move'
-      })
+      });
     } else if (this.isMouseDown && this.state.isDrawLineinProg) {
-      this.setState({
-        currDrawLinePts: [this.state.currDrawLinePts[0], { x: e.pageX, y: e.pageY }]
-      }, () => console.log(this.state.currDrawLinePts))
-      this.lastMousePos = { x: e.pageX, y: e.pageY }
+      this.setState(
+        {
+          currDrawLinePts: [
+            this.state.currDrawLinePts[0],
+            { x: e.pageX, y: e.pageY }
+          ]
+        },
+        () => console.log(this.state.currDrawLinePts)
+      );
+      this.lastMousePos = { x: e.pageX, y: e.pageY };
     }
-  }
+  };
 
-  mouseLeave = (e) => {
+  mouseLeave = e => {
     this.isMouseDown = false;
-  }
-
-
-
+  };
 
   onClickDrawLine = () => {
-    this.setState({
-      isDrawLineinProg: true,
-      isScaleSetInProg: false,
-    }, () => {
-      console.log('draw lin in progress')
+    this.setState({ isDrawLineinProg: true, isScaleSetInProg: false }, () => {
+      console.log('draw lin in progress');
     });
-
-  }
-
+  };
 
   render() {
-    return (<>
-      <Sidebar
-        handleFileUpload={this.handleFileUpload}
-        onClickScalebarBtn={this.onClickScalebarBtn}
-        onSaveSnapClicked={this.onSaveSnapClicked}
-        snapUrls={this.state.snapUrls}
-        imageLoaded={this.state.imageLoaded}
-        scalePtsLength={this.state.scalePts.length}
-        onCheckUseScalebar={this.onCheckUseScalebar}
-        isScalebarChecked={this.state.isScalebarChecked}
-        isImageScaleSet={this.state.isImageScaleSet}
-        onInputLengthChange={this.onInputLengthChange}
-        onInputUnitsChange={this.onInputUnitsChange}
-        inputLengthValue={this.state.inputLengthValue}
-        inputUnitsValue={this.state.inputUnitsValue}
-        onClickDoneSetting={this.onClickDoneSetting}
-        onClickSetImageScale={this.onClickSetImageScale}
-        selectedFile={this.state.selectedFile}
-        containerRef={this.state.containerRef}
-        size={this.state.size}
-        pos={this.state.pos}
-        origDims={this.state.origDims}
-        useDemoUpload={this.useDemoUpload}
-        isScaleSetInProg={this.state.isScaleSetInProg}
-        onClickCancelSetting={this.onClickCancelSetting}
-        onClickScaleTextColor={this.onClickScaleTextColor}
-        onClickScaleBgColor={this.onClickScaleBgColor}
-        isDrawLineinProg={this.isDrawLineinProg}
-        onClickDrawLine={this.onClickDrawLine}
-
-
-
-
-
-      />
-      <Micrograph
-        selectedFile={this.state.selectedFile}
-        getContainerRef={this.getContainerRef}
-        size={this.state.size}
-        pos={this.state.pos}
-        imgSizeUnits={this.state.imgSizeUnits}
-        imageLoaded={this.state.imageLoaded}
-        onMouseScroll={this.onMouseScroll}
-        mouseDown={this.mouseDown}
-        mouseUp={this.mouseUp}
-        mouseMove={this.mouseMove}
-        mouseLeave={this.mouseLeave}
-        mouseEnter={this.mouseEnter}
-        isImageScaleSet={this.state.isImageScaleSet}
-        isScalebarChecked={this.state.isScalebarChecked}
-        units={this.state.units}
-        scalebarTextColor={this.state.scalebarTextColor}
-        scalebarBgColor={this.state.scalebarBgColor}
-        isScaleSetInProg={this.state.isScaleSetInProg}
-        scalePtsLength={this.state.scalePts.length}
-        cursorStyle={this.state.cursorStyle}
-      />
-    </>)
+    return (
+      <>
+        <Sidebar
+          handleFileUpload={this.handleFileUpload}
+          onClickScalebarBtn={this.onClickScalebarBtn}
+          onSaveSnapClicked={this.onSaveSnapClicked}
+          snapUrls={this.state.snapUrls}
+          imageLoaded={this.state.imageLoaded}
+          scalePtsLength={this.state.scalePts.length}
+          onCheckUseScalebar={this.onCheckUseScalebar}
+          isScalebarChecked={this.state.isScalebarChecked}
+          isImageScaleSet={this.state.isImageScaleSet}
+          onInputLengthChange={this.onInputLengthChange}
+          onInputUnitsChange={this.onInputUnitsChange}
+          inputLengthValue={this.state.inputLengthValue}
+          inputUnitsValue={this.state.inputUnitsValue}
+          onClickDoneSetting={this.onClickDoneSetting}
+          onClickSetImageScale={this.onClickSetImageScale}
+          selectedFile={this.state.selectedFile}
+          containerRef={this.state.containerRef}
+          size={this.state.size}
+          pos={this.state.pos}
+          origDims={this.state.origDims}
+          useDemoUpload={this.useDemoUpload}
+          isScaleSetInProg={this.state.isScaleSetInProg}
+          onClickCancelSetting={this.onClickCancelSetting}
+          onClickScaleTextColor={this.onClickScaleTextColor}
+          onClickScaleBgColor={this.onClickScaleBgColor}
+          isDrawLineinProg={this.isDrawLineinProg}
+          onClickDrawLine={this.onClickDrawLine}
+        />
+        <Micrograph
+          selectedFile={this.state.selectedFile}
+          getContainerRef={this.getContainerRef}
+          size={this.state.size}
+          pos={this.state.pos}
+          imgSizeUnits={this.state.imgSizeUnits}
+          imageLoaded={this.state.imageLoaded}
+          onMouseScroll={this.onMouseScroll}
+          mouseDown={this.mouseDown}
+          mouseUp={this.mouseUp}
+          mouseMove={this.mouseMove}
+          mouseLeave={this.mouseLeave}
+          mouseEnter={this.mouseEnter}
+          isImageScaleSet={this.state.isImageScaleSet}
+          isScalebarChecked={this.state.isScalebarChecked}
+          units={this.state.units}
+          scalebarTextColor={this.state.scalebarTextColor}
+          scalebarBgColor={this.state.scalebarBgColor}
+          isScaleSetInProg={this.state.isScaleSetInProg}
+          scalePtsLength={this.state.scalePts.length}
+          cursorStyle={this.state.cursorStyle}
+        />
+      </>
+    );
   }
 }
 
